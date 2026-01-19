e466e7d83b4c411c9dbbbfd8fc7208e12cdb7f41
DatGuy64 <duke.herpe@gmail.com>
2026-01-19 01:41:34 +0100

    Revert "test"
    This reverts commit 80c6d37b41d3192c93e5dc85bf64bb5bf81f243e.

--- source/modules/networking.cpp
+++ source/modules/networking.cpp
@@ -1884,6 +1884,8 @@ static ConVar networking_fastpath_usecluster("holylib_networking_fastpath_useclu
 
 static inline bool HolyPVS_AWHSeenTest(int viewerSlot, int targetSlot)
 {
+    if (targetSlot < 1 || targetSlot > HOLYLIB_MAX_PLAYERS)
+        return true;
     const int bit = targetSlot - 1;
     const int word = (bit >> 6);
     const uint64_t mask = 1ULL << (bit & 63);
@@ -1892,6 +1894,8 @@ static inline bool HolyPVS_AWHSeenTest(int viewerSlot, int targetSlot)
 
 static inline void HolyPVS_AWHSeenSet(int viewerSlot, int targetSlot)
 {
+    if (targetSlot < 1 || targetSlot > HOLYLIB_MAX_PLAYERS)
+        return;
     const int bit = targetSlot - 1;
     const int word = (bit >> 6);
     const uint64_t mask = 1ULL << (bit & 63);
@@ -1900,58 +1904,53 @@ static inline void HolyPVS_AWHSeenSet(int viewerSlot, int targetSlot)
 
 static inline void ApplyAntiWallhackFastTransmit(CBasePlayer* viewer, int viewerSlot, CCheckTransmitInfo* pInfo)
 {
-    if (!g_HolyPVS_AWHEnabled[viewerSlot])
-        return;
+	if (!g_HolyPVS_AWHEnabled[viewerSlot])
+		return;
 
-    const float cacheSeconds = g_HolyPVS_AWHCacheSeconds[viewerSlot];
-    const int maxClients = gpGlobals->maxClients;
+	//VPROF_BUDGET("HolyLib - AntiWallhack", VPROF_BUDGETGROUP_OTHER_NETWORKING);
 
-    CBitVec<MAX_EDICTS>* pTransmitBits = pInfo->m_pTransmitEdict;
-    CBitVec<MAX_EDICTS>* pAlwaysBits   = pInfo->m_pTransmitAlways;
-    const bool hasAlways = (pAlwaysBits != nullptr);
+	const float cacheSeconds = g_HolyPVS_AWHCacheSeconds[viewerSlot];
+	const int maxClients = gpGlobals->maxClients;
 
-    for (int i = pTransmitBits->FindNextSetBit(1);
-         i != -1 && i <= maxClients;
-         i = pTransmitBits->FindNextSetBit(i + 1))
-    {
-        if (i == viewerSlot)
-            continue;
+	CBitVec<MAX_EDICTS>* pTransmitBits = pInfo->m_pTransmitEdict;
+	CBitVec<MAX_EDICTS>* pAlwaysBits = pInfo->m_pTransmitAlways;
 
-        CBaseEntity* targetEnt = g_pEntityCache[i];
-        if (!targetEnt)
-            continue;
+	for (int i = 1; i <= maxClients; ++i)
+	{
+		if (i == viewerSlot) continue;
+		if (!pTransmitBits->Get(i)) continue;
 
-        if (!HolyPVS_AWHSeenTest(viewerSlot, i))
-        {
-            HolyPVS_AWHSeenSet(viewerSlot, i);
-            continue;
-        }
+		CBaseEntity* targetEnt = g_pEntityCache[i];
+		if (!targetEnt) continue;
 
-        if (!HolyPVS_VisibleByLOS(viewer, targetEnt, cacheSeconds))
-        {
-            pTransmitBits->Clear(i);
-            if (hasAlways) pAlwaysBits->Clear(i);
+		if (!HolyPVS_AWHSeenTest(viewerSlot, i))
+		{
+			HolyPVS_AWHSeenSet(viewerSlot, i);
+			continue;
+		}
 
-            for (CBaseEntity* ch = targetEnt->FirstMoveChild(); ch; ch = ch->NextMovePeer())
-            {
-                edict_t* chEd = ch->edict();
-                if (!chEd)
-                    continue;
+		if (!HolyPVS_VisibleByLOS(viewer, targetEnt, cacheSeconds))
+		{
+			pTransmitBits->Clear(i);
+			if (pAlwaysBits) pAlwaysBits->Clear(i);
 
-                const int idx = chEd->m_EdictIndex;
-                if (idx <= maxClients)
-                    continue;
+			for (CBaseEntity* ch = targetEnt->FirstMoveChild(); ch; ch = ch->NextMovePeer())
+			{
+				edict_t* chEd = ch->edict();
+				if (!chEd) continue;
 
-                if (pTransmitBits->Get(idx))
-                {
-                    pTransmitBits->Clear(idx);
-                    if (hasAlways) pAlwaysBits->Clear(idx);
-                }
-            }
-        }
-    }
+				const int idx = chEd->m_EdictIndex;
+				if (idx <= maxClients) continue;
+
+				if (pTransmitBits->Get(idx))
+				{
+					pTransmitBits->Clear(idx);
+					if (pAlwaysBits) pAlwaysBits->Clear(idx);
+				}
+			}
+		}
+	}
 }
-
 
 bool New_CServerGameEnts_CheckTransmit(IServerGameEnts* gameents, CCheckTransmitInfo *pInfo, const unsigned short *pEdictIndices, int nEdicts)
 {
