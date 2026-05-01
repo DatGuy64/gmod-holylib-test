390fe964e8e039e33f558831d457422cbc7df1b2
Nalo <bryandve@hotmail.fr>
2026-05-01 18:29:09 +0200

    tt

--- source/modules/networking.cpp
+++ source/modules/networking.cpp
@@ -2524,6 +2524,7 @@ void PackEntities_Normal(int clientCount, CGameClient **clients, CFrameSnapshot 
 		for (int iClient = 0; iClient < clientCount; ++iClient)
 		{
 			const CClientFrame *frame = clients[iClient]->m_pCurrentFrame;
+			if (!frame) continue;
 			for (int iValidEdict = 0; iValidEdict < snapshot->m_nValidEntities; ++iValidEdict)
 			{
 				const int index = snapshot->m_pValidEntities[iValidEdict];
