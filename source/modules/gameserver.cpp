7de7af24689574d0ffda24aa55f679247d1d8b76
Nalo <bryandve@hotmail.fr>
2026-05-05 00:15:48 +0200

    a

--- source/modules/gameserver.cpp
+++ source/modules/gameserver.cpp
@@ -2960,7 +2960,7 @@ static void hook_CBaseServer_CheckTimeouts(CBaseServer* srv)
 	int i;
 
 #if !defined( _DEBUG )
-	for (i=0 ; i< srv->GetClientCount() ; i++ )
+	for (i=0 ; i< srv->m_Clients.Count() ; i++ )
 	{
 		IClient	*cl = srv->GetClient(i);
 
@@ -2993,7 +2993,7 @@ static void hook_CBaseServer_CheckTimeouts(CBaseServer* srv)
 	}
 #endif
 
-	for (i=0 ; i< srv->GetClientCount() ; i++ )
+	for (i=0 ; i< srv->m_Clients.Count() ; i++ )
 	{
 		IClient	*cl = srv->GetClient(i);
 
