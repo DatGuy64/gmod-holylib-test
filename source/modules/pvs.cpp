adcd6af86cca1faafaaca30652851af4bb007db2
DatGuy64 <duke.herpe@gmail.com>
2026-05-26 20:16:37 +0200

    trace test

--- source/modules/pvs.cpp
+++ source/modules/pvs.cpp
@@ -52,7 +52,7 @@ static inline bool LOS_Clear(const Vector& start, const Vector& end)
     trace_t tr;
     Ray_t ray;
     ray.Init(start, end);
-    enginetrace->TraceRay(ray, MASK_OPAQUE_AND_NPCS, &g_HolyLibTraceFilterWorldOnly, &tr);
+	enginetrace->TraceRay(ray, MASK_OPAQUE | CONTENTS_IGNORE_NODRAW_OPAQUE, &g_HolyLibTraceFilterWorldOnly, &tr);
     return tr.fraction > 0.97f;
 }
 
